ada614643c16d9de4bbf28dd0f68cfd2e12dd00f
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-05 21:42:20 +0600

    add all the functionalist of hidden search bar

--- day-4/script.js
+++ day-4/script.js
@@ -1,3 +1,12 @@
 'use strict';
 console.log('working ....');
 
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+
+console.log(btn);
+btn.addEventListener('click', () => {
+	search.classList.toggle('active');
+	input.focus();
+});
