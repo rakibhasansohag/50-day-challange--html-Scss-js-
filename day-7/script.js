79c68696d9dca93c83b697b43d6a06764b79a07e
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-07 20:07:22 +0600

    add project functionalist

--- day-7/script.js
+++ day-7/script.js
@@ -1,2 +1,22 @@
 'use strict';
 console.log('working ....');
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', (e) =>
+	container.classList.add('hover-left'),
+);
+
+right.addEventListener('mouseenter', (e) =>
+	container.classList.add('hover-right'),
+);
+
+left.addEventListener('mouseleave', (e) =>
+	container.classList.remove('hover-left'),
+);
+
+right.addEventListener('mouseleave', (e) =>
+	container.classList.remove('hover-right'),
+);
