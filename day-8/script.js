b065b722ea1a0f6b7372ad595e997f8d4361f112
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-08 18:06:42 +0600

    add function and animation to the wave

--- day-8/script.js
+++ day-8/script.js
@@ -1,2 +1,14 @@
 'use strict';
 console.log('working ....');
+
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach((label) => {
+	label.innerHTML = label.innerText
+		.split('')
+		.map(
+			(letter, id) =>
+				`<span style="transition-delay:${id * 100}ms">${letter}</span>`,
+		)
+		.join('');
+});
