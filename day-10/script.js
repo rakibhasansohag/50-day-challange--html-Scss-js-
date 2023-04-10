2f270edd6000f495216d4eb2abf88861c3059d7b
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-10 18:59:22 +0600

    add all the function

--- day-10/script.js
+++ day-10/script.js
@@ -2,4 +2,23 @@
 console.log('working ....');
 
 // api for the project
-const api = 'https://icanhazdadjoke.com/';
+const api = 'https://icanhazdadjoke.com';
+
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+const generateJoke = function () {
+	const config = {
+		headers: {
+			Accept: 'application/json',
+		},
+	};
+	fetch(api, config)
+		.then((res) => res.json())
+		.then((data) => {
+			jokeEl.innerHTML = data.joke;
+		});
+};
+generateJoke();
+
+jokeBtn.addEventListener('click', generateJoke);
