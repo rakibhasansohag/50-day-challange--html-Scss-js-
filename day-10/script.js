5d731be4da4c277131d367aa89e8ba5c2cd13a4f
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-10 19:04:30 +0600

    using async await

--- day-10/script.js
+++ day-10/script.js
@@ -7,18 +7,35 @@ const api = 'https://icanhazdadjoke.com';
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
-const generateJoke = function () {
+// using Fetch API
+// const generateJoke = function () {
+// 	const config = {
+// 		headers: {
+// 			Accept: 'application/json',
+// 		},
+// 	};
+// 	// using fetch
+// 	fetch(api, config)
+// 		.then((res) => res.json())
+// 		.then((data) => {
+// 			jokeEl.innerHTML = data.joke;
+// 		});
+// };
+
+// using Async Await
+const generateJoke = async function () {
 	const config = {
 		headers: {
 			Accept: 'application/json',
 		},
 	};
-	fetch(api, config)
-		.then((res) => res.json())
-		.then((data) => {
-			jokeEl.innerHTML = data.joke;
-		});
+
+	const res = await fetch(api, config);
+	const data = await res.json();
+
+	jokeEl.innerHTML = data.joke;
 };
+
 generateJoke();
 
 jokeBtn.addEventListener('click', generateJoke);
