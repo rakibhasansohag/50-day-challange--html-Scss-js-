929a4f2f9b03634d23fe67cb9aaf8e7c346ff737
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-13 18:01:17 +0600

    add function on creating tags and tag

--- day-13/script.js
+++ day-13/script.js
@@ -1,3 +1,27 @@
 'use strict';
 console.log('working ....');
 
+const tagsEl = document.querySelector('.tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+
+textarea.addEventListener('keyup', (e) => {
+	createTagsFunction(e.target.value);
+});
+
+function createTagsFunction(input) {
+	const tags = input
+		.split(',')
+		.filter((tag) => tag.trim() !== '')
+		.map((tag) => tag.trim());
+	// console.log(tags);
+	tagsEl.innerHTML = '';
+
+	tags.forEach((tag) => {
+		const tagEl = document.createElement('span');
+		tagEl.classList.add('tag');
+		tagEl.innerText = tag;
+		tagsEl.appendChild(tagEl);
+	});
+}
