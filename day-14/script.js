c13ffccaef92040cd4bdd8acffc7c3d79cb4ea7b
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-15 00:02:16 +0600

    bug fix on line

--- day-14/script.js
+++ day-14/script.js
@@ -1,2 +1,9 @@
 'use strict';
 console.log('working ....');
+
+const toggle = document.getElementById('toggle');
+const nav = document.getElementById('nav');
+
+toggle.addEventListener('click', () => {
+	nav.classList.toggle('active');
+});
