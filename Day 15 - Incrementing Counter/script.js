e2162acbe9dca3152d85cc86c449bec38c998043
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-15 19:25:28 +0600

    add all the funciton for increment counter

--- Day 15 - Incrementing Counter/script.js
+++ Day 15 - Incrementing Counter/script.js
@@ -1,2 +1,26 @@
 'use strict';
 console.log('working ....');
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+	counter.innerText = '0';
+
+	const updateCounter = () => {
+		const target = +counter.getAttribute('data-target');
+		// console.log(typeof target, target);
+		const c = +counter.innerText;
+		const increment = target / 200;
+		// console.log(increment);
+
+		if (c < target) {
+			counter.innerText = `${Math.round(c + increment)}`;
+
+			setTimeout(updateCounter, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCounter();
+});
