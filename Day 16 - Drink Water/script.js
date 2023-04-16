1c35b340abc10290f3cffc79ef51a1db8a6e50a4
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-17 02:56:23 +0600

    add funciton for small cups

--- Day 16 - Drink Water/script.js
+++ Day 16 - Drink Water/script.js
@@ -1,3 +1,30 @@
 'use strict';
 console.log('working ....');
 
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+
+const remained = document.querySelector('.remained');
+
+smallCups.forEach((cup, id) => {
+	cup.addEventListener('click', () => highlightCups(id));
+});
+
+function highlightCups(id) {
+	if (
+		smallCups[id].classList.contains('full') &&
+		!smallCups[id].nextElementSibling.classList.contains('full')
+	) {
+		id--;
+	}
+
+	// console.log(id);
+	smallCups.forEach((cup, id2) => {
+		if (id2 <= id) {
+			cup.classList.add('full');
+		} else {
+			cup.classList.remove('full');
+		}
+	});
+}
