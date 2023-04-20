b6da5346a61b1832f1e81506ea1188475a15bc3f
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-21 01:20:07 +0600

    add all the funciton for this project

--- Day 20 - Button Ripple Effect/script.js
+++ Day 20 - Button Ripple Effect/script.js
@@ -1,2 +1,37 @@
 'use strict';
 console.log('working ....');
+
+// task : 1 for getting click event
+//  variable
+const clickButton = document.querySelectorAll('.click');
+
+// console.log(clickButton);
+
+clickButton.forEach((button) => {
+	button.addEventListener('click', function (event) {
+		const x = event.clientX;
+		const y = event.clientY;
+		// console.log(x, y); /// for testing  x,y
+
+		const buttonTop = event.target.offsetTop;
+		const buttonLeft = event.target.offsetLeft;
+
+		// console.log(buttonTop, buttonLeft); /// for testing  buttonTop,buttonLeft
+
+		const xInside = x - buttonLeft;
+		const yInside = y - buttonTop;
+
+		// console.log(xInside, yInside); /// for testing  xInside,yInside
+
+		const circle = document.createElement('span');
+
+		circle.classList.add('circle');
+		circle.style.top = yInside + 'px';
+		circle.style.left = xInside + 'px';
+
+		// console.log(circle); /// for testing  circle
+		this.appendChild(circle);
+
+		setTimeout(() => circle.remove(), 500);
+	});
+});
