0db05609b17926ffe0e7b75c08064ca211ec6395
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-21 01:44:08 +0600

    fix a bug not working the click

--- Day 20 - Button Ripple Effect/script.js
+++ Day 20 - Button Ripple Effect/script.js
@@ -3,35 +3,26 @@ console.log('working ....');
 
 // task : 1 for getting click event
 //  variable
-const clickButton = document.querySelectorAll('.click');
+const buttons = document.querySelectorAll('.click');
 
-// console.log(clickButton);
+buttons.forEach((button) => {
+	button.addEventListener('click', function (e) {
+		const x = e.pageX;
+		const y = e.pageY;
 
-clickButton.forEach((button) => {
-	button.addEventListener('click', function (event) {
-		const x = event.clientX;
-		const y = event.clientY;
-		// console.log(x, y); /// for testing  x,y
-
-		const buttonTop = event.target.offsetTop;
-		const buttonLeft = event.target.offsetLeft;
-
-		// console.log(buttonTop, buttonLeft); /// for testing  buttonTop,buttonLeft
+		const buttonTop = e.target.offsetTop;
+		const buttonLeft = e.target.offsetLeft;
 
 		const xInside = x - buttonLeft;
 		const yInside = y - buttonTop;
 
-		// console.log(xInside, yInside); /// for testing  xInside,yInside
-
 		const circle = document.createElement('span');
-
 		circle.classList.add('circle');
 		circle.style.top = yInside + 'px';
 		circle.style.left = xInside + 'px';
 
-		// console.log(circle); /// for testing  circle
 		this.appendChild(circle);
 
-		setTimeout(() => circle.remove(), 500);
+		setTimeout(() => circle.remove(), 1000);
 	});
 });
