6ef584d1cbab842503d0e2c24ffcca0e2c52fcfe
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-25 23:23:38 +0600

    add nav style and function

--- Day 25 - Sticky Navbar/script.js
+++ Day 25 - Sticky Navbar/script.js
@@ -1,2 +1,23 @@
 'use strict';
 console.log('working ....');
+
+// point all the variables
+
+const nav = document.querySelector('.nav');
+
+// point event listener
+window.addEventListener('scroll', fixName);
+
+// point fix Now function
+
+function fixName() {
+	console.log('scrolling ....');
+	console.log(window.scrollY, nav.offsetHeight);
+
+	// / 120 is for if the user scroll down 194px then the nav will be fixed ( 120 + 74) = 194
+	if (window.scrollY > nav.offsetHeight + 120) {
+		nav.classList.add('active');
+	} else {
+		nav.classList.remove('active');
+	}
+}
