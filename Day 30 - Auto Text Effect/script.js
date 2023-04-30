fc348c77d80667cbe3757fa788af244c900e78bb
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-30 09:34:52 +0600

    add more text on the auto text effect

--- Day 30 - Auto Text Effect/script.js
+++ Day 30 - Auto Text Effect/script.js
@@ -6,18 +6,35 @@ const textEl = document.getElementById('text');
 const speedEl = document.getElementById('speed');
 
 // text to be displayed
-const text = 'Hello , I am a web developer';
+const texts = [
+	'Hello , I am a web developer',
+	'And My Name Is Rakib Hasan Sohag',
+	'I love to code',
+	'I am Passionate about coding',
+	'I am a self taught developer',
+	'And  I am still learning',
+];
 
 // function to display the text
 let id = 1;
-let speed = 300 / speedEl.value;
+let speed = 100 / speedEl.value;
 
+// new features
+let textIndex = 0;
+let text = texts[textIndex];
+
+// function to write the text
 function writeText() {
 	textEl.innerText = text.slice(0, id);
 	id++;
 
 	if (id > text.length) {
 		id = 1;
+		textIndex++;
+		if (textIndex > texts.length - 1) {
+			textIndex = 0;
+		}
+		text = texts[textIndex];
 	}
 	setTimeout(writeText, speed); // recursive function
 }
