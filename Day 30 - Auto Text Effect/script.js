bf1e3fb4c1e11cdab7e0028ab24b128a60e1728e
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-04-30 09:21:32 +0600

    add all the style and function for auto text

--- Day 30 - Auto Text Effect/script.js
+++ Day 30 - Auto Text Effect/script.js
@@ -1,2 +1,30 @@
 'use strict';
 console.log('working ....');
+
+//  all the variables
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+
+// text to be displayed
+const text = 'Hello , I am a web developer';
+
+// function to display the text
+let id = 1;
+let speed = 300 / speedEl.value;
+
+function writeText() {
+	textEl.innerText = text.slice(0, id);
+	id++;
+
+	if (id > text.length) {
+		id = 1;
+	}
+	setTimeout(writeText, speed); // recursive function
+}
+
+writeText();
+
+// event listener for speed
+speedEl.addEventListener('input', (e) => {
+	speed = 300 / e.target.value;
+});
