3f7a02fd0f632140e2e88069bf76857e3f934f38
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-02 09:40:41 +0600

    add animaiton and conddition on checkbox

--- Day 32 - Good, Cheap, Fast Checkboxes/script.js
+++ Day 32 - Good, Cheap, Fast Checkboxes/script.js
@@ -1,2 +1,29 @@
 'use strict';
 console.log('working ....');
+
+// point : declaration all the variables
+
+const toggles = document.querySelectorAll('.toggle');
+const good = document.querySelector('#good');
+const cheap = document.querySelector('#cheap');
+const fast = document.querySelector('#fast');
+
+// point : function to check the toggle
+function doTheTrick(theClickedOne) {
+	if (good.checked && cheap.checked && fast.checked) {
+		if (good === theClickedOne) {
+			fast.checked = false;
+		}
+		if (cheap === theClickedOne) {
+			good.checked = false;
+		}
+		if (fast === theClickedOne) {
+			cheap.checked = false;
+		}
+	}
+}
+
+// point : event listener
+toggles.forEach((toggle) =>
+	toggle.addEventListener('change', (e) => doTheTrick(e.target)),
+);
