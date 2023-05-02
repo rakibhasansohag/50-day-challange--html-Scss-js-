9eb4fa31bd9321382511433130ec0b3bb4b3e127
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-02 09:59:46 +0600

    add reset button and project summery

--- Day 32 - Good, Cheap, Fast Checkboxes/script.js
+++ Day 32 - Good, Cheap, Fast Checkboxes/script.js
@@ -7,6 +7,8 @@ const toggles = document.querySelectorAll('.toggle');
 const good = document.querySelector('#good');
 const cheap = document.querySelector('#cheap');
 const fast = document.querySelector('#fast');
+const resetBtn = document.querySelector('.reset');
+const summery = document.querySelector('#summery');
 
 // point : function to check the toggle
 function doTheTrick(theClickedOne) {
@@ -27,3 +29,36 @@ function doTheTrick(theClickedOne) {
 toggles.forEach((toggle) =>
 	toggle.addEventListener('change', (e) => doTheTrick(e.target)),
 );
+
+// point : add reset button to reset all the toggles
+resetBtn.addEventListener('click', () => {
+	toggles.forEach((toggle) => {
+		toggle.checked = false;
+		toggle.nextElementSibling.classList.remove('is-checked');
+		summery.innerText = 'You Chose Reset';
+	});
+});
+
+// point : for the summery of the project
+function updateSummary() {
+	const choices = [];
+	if (good.checked) {
+		choices.push('Good');
+	}
+	if (cheap.checked) {
+		choices.push('Cheap');
+	}
+	if (fast.checked) {
+		choices.push('Fast');
+	}
+	summery.innerText = `You Chose ${choices.join(', ')}`;
+}
+
+toggles.forEach((toggle) => {
+	toggle.addEventListener('change', () => {
+		doTheTrick(toggle);
+		updateSummary();
+	});
+});
+
+updateSummary();
