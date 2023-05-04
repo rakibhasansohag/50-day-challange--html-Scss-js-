93a97e7169c4e9767b6ad3d64ef9f846e7d38615
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-04 10:24:52 +0600

    add all the funtion for the animation

---
+++ Day 34 - Animated Countdown/script.js
@@ -0,0 +1,43 @@
+'use strict';
+console.log('working ....');
+
+// point : all the variable
+const nums = document.querySelectorAll('.num span');
+const counter = document.querySelector('.counter');
+const finalMessage = document.querySelector('.final');
+const replay = document.querySelector('#replay');
+
+// point for the animation
+runAnimation();
+
+function runAnimation() {
+	nums.forEach((num, id) => {
+		const nextToLast = nums.length - 1;
+		num.addEventListener('animationend', (e) => {
+			if (e.animationName === 'goIn' && id !== nextToLast) {
+				num.classList.remove('in');
+				num.classList.add('out');
+			} else if (e.animationName === 'goOut' && num.nextElementSibling) {
+				num.nextElementSibling.classList.add('in');
+			} else {
+				counter.classList.add('hide');
+				finalMessage.classList.add('show');
+			}
+		});
+	});
+}
+
+//point : for the replay button
+function replyBtn() {
+	counter.classList.remove('hide');
+	finalMessage.classList.remove('show');
+	nums.forEach((num) => {
+		num.classList.value = '';
+	});
+	nums[0].classList.add('in');
+}
+
+replay.addEventListener('click', () => {
+	replyBtn();
+	runAnimation();
+});
