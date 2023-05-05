c0551f0a4054fbce2e1480f59c316461130464d5
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-05 10:51:52 +0600

    add responsiveness and add 2 featres on key and hover puse and play the slider

--- Day 35 - Image Carousel/script.js
+++ Day 35 - Image Carousel/script.js
@@ -6,6 +6,11 @@ const imgs = document.getElementById('imgs');
 const leftBtn = document.getElementById('left');
 const rightBtn = document.getElementById('right');
 const img = document.querySelectorAll('#imgs img');
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+const img4 = document.getElementById('img4');
+const img5 = document.getElementById('img5');
 
 // point : all the functionality
 
@@ -44,4 +49,39 @@ leftBtn.addEventListener('click', () => {
 	id--;
 	changeImage();
 	resetInterval();
+});
+
+// point : function for pause and play
+
+function pauseAutoSlide() {
+	clearInterval(interval);
+}
+function resumeAutoSlide() {
+	interval = setInterval(run, 2000);
+}
+// / for pause
+img1.addEventListener('mouseover', pauseAutoSlide);
+img2.addEventListener('mouseover', pauseAutoSlide);
+img3.addEventListener('mouseover', pauseAutoSlide);
+img4.addEventListener('mouseover', pauseAutoSlide);
+img5.addEventListener('mouseover', pauseAutoSlide);
+
+// / for resume
+img1.addEventListener('mouseout', resumeAutoSlide);
+img2.addEventListener('mouseout', resumeAutoSlide);
+img3.addEventListener('mouseout', resumeAutoSlide);
+img4.addEventListener('mouseout', resumeAutoSlide);
+img5.addEventListener('mouseout', resumeAutoSlide);
+
+// point : function for keyboard
+document.addEventListener('keydown', (e) => {
+	if (e.key === 'ArrowRight') {
+		id++;
+		changeImage();
+		resetInterval();
+	} else if (e.key === 'ArrowLeft') {
+		id--;
+		changeImage();
+		resetInterval();
+	}
 });
