50f0a586a410985cf62916b23807603047ee0850
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-05 09:52:37 +0600

    add all the funtion

--- Day 35 - Image Carousel/script.js
+++ Day 35 - Image Carousel/script.js
@@ -1,2 +1,47 @@
 'use strict';
 console.log('working ....');
+
+// point : all the variables
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+const img = document.querySelectorAll('#imgs img');
+
+// point : all the functionality
+
+let id = 0;
+let interval = setInterval(run, 2000);
+function resetInterval() {
+	clearInterval(interval);
+	interval = setInterval(run, 2000);
+}
+
+// point : all the functions
+function run() {
+	id++;
+
+	changeImage();
+}
+
+function changeImage() {
+	if (id > img.length - 1) {
+		id = 0;
+	} else if (id < 0) {
+		id = img.length - 1;
+	}
+
+	imgs.style.transform = `translateX(${-id * 700}px)`; // / : 700px is the width of the image
+}
+
+// point : all the event listeners
+rightBtn.addEventListener('click', () => {
+	id++;
+	changeImage();
+	resetInterval();
+});
+
+leftBtn.addEventListener('click', () => {
+	id--;
+	changeImage();
+	resetInterval();
+});
