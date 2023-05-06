9cd9ab8f114cb623765c6c4d2408495d048ec270
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-06 11:26:19 +0600

    add all the funtion

--- Day 36 - Hoverboard/script.js
+++ Day 36 - Hoverboard/script.js
@@ -3,3 +3,35 @@ console.log('working ....');
 
 // point : all the variable
 
+const container = document.querySelector('.container');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+const SQUARES = 500;
+
+// point : all the function
+for (let i = 0; i < SQUARES; i++) {
+	const square = document.createElement('div');
+	square.classList.add('square');
+
+	square.addEventListener('mouseover', () => setColor(square));
+	square.addEventListener('mouseout', () => removeColor(square));
+
+	container.appendChild(square);
+}
+
+// point set color
+function setColor(element) {
+	const color = getRandomColor();
+	element.style.background = color;
+	element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+// point remove color
+function removeColor(element) {
+	element.style.background = '#1d1d1d'; /// primary color
+	element.style.boxShadow = '0 0 2px #000'; /// primary shadow
+}
+
+// point : get random color
+function getRandomColor() {
+	return colors[Math.floor(Math.random() * colors.length)];
+}
