eff08bea3f7e47f3ca97f30f6a10787f9144a89a
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-07 00:16:37 +0600

    added add event functions

--- Day 1 - Calendar with todo/script.js
+++ Day 1 - Calendar with todo/script.js
@@ -13,7 +13,7 @@ const dateInput = document.querySelector('.date-input');
 
 // point :
 let today = new Date();
-console.log(today);
+// console.log(today);
 let activeDay;
 let month = today.getMonth();
 let year = today.getFullYear();
@@ -32,6 +32,8 @@ const months = [
 	'November',
 	'December',
 ];
+
+
 
 // point : function to add days
 function initCalendar() {
@@ -57,6 +59,14 @@ function initCalendar() {
 
 	// point : add current months days
 	for (let i = 1; i <= lastDate; i++) {
+
+
+		
+
+
+
+
+
 		// point : add active class to current day
 
 		if (
@@ -158,3 +168,74 @@ function gotoDate() {
 }
 
 /// task for right part of the calendar
+
+// point : all the variables
+const addEventBtn = document.querySelector('.add-event');
+const addEventContainer = document.querySelector('.add-event-wrapper');
+const addEventCloseBtn = document.querySelector('.close');
+const addEventName = document.querySelector('.event-name');
+const addEventFrom = document.querySelector('.event-time-from');
+const addEventTo = document.querySelector('.event-time-to');
+
+// point : add event listener to add event button
+addEventBtn.addEventListener('click', () => {
+	addEventContainer.classList.toggle('active');
+});
+
+// point : add event listener to close button
+addEventCloseBtn.addEventListener('click', () => {
+	addEventContainer.classList.remove('active');
+});
+
+// point : add event listener to document (to close add event container when clicked outside)
+document.addEventListener('click', (e) => {
+	if (e.target !== addEventBtn && !addEventContainer.contains(e.target)) {
+		addEventContainer.classList.remove('active');
+	}
+});
+
+// allow only 50 chars in event name
+addEventName.addEventListener('input', (e) => {
+	if (e.target.value.length > 50) {
+		e.target.value = e.target.value.slice(0, 50);
+	}
+});
+
+// time validation
+addEventFrom.addEventListener('input', (e) => {
+	// point : allow only numbers
+	addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, '');
+	// point : check if time is valid
+	if (addEventFrom.value.length === 2) {
+		addEventFrom.value += ':';
+	}
+	// point : don't late the user to enter more than 5 chars
+	if (addEventFrom.value.length > 5) {
+		addEventFrom.value = addEventFrom.value.slice(0, 5);
+	}
+	// point : if backspace pressed
+	if (e.inputType === 'deleteContentBackward') {
+		if (addEventFrom.value.length === 3) {
+			addEventFrom.value = addEventFrom.value.slice(0, 2);
+		}
+	}
+});
+
+addEventTo.addEventListener('input', (e) => {
+	// point : allow only numbers
+	addEventTo.value = addEventTo.value.replace(/[^0-9:]/g, '');
+	// point : check if time is valid
+	if (addEventTo.value.length === 2) {
+		addEventTo.value += ':';
+	}
+	// point : don't late the user to enter more than 5 chars
+	if (addEventTo.value.length > 5) {
+		addEventTo.value = addEventTo.value.slice(0, 5);
+	}
+	// point : if backspace pressed
+	if (e.inputType === 'deleteContentBackward') {
+		if (addEventTo.value.length === 3) {
+			addEventTo.value = addEventTo.value.slice(0, 2);
+		}
+	}
+});
