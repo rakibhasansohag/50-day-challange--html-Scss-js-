c1f2c7074b05d31fd8b01116f08e6eb3e82bf9ad
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-08 09:55:41 +0600

    add content fucntion

--- Day 38 - Mobile Tab Navigation/script.js
+++ Day 38 - Mobile Tab Navigation/script.js
@@ -1,2 +1,29 @@
 'use strict';
 console.log('working ....');
+
+// point : all the variables for this global
+const contents = document.querySelectorAll('.content');
+const listItems = document.querySelectorAll('nav ul li');
+
+// point : all the functions for this global
+listItems.forEach((item, id) => {
+	item.addEventListener('click', () => {
+		hideAllContents();
+		hideAllItems();
+
+		item.classList.add('active');
+		contents[id].classList.add('show');
+	});
+});
+
+function hideAllContents() {
+	contents.forEach((content) => {
+		content.classList.remove('show');
+	});
+}
+
+function hideAllItems() {
+	listItems.forEach((item) => {
+		item.classList.remove('active');
+	});
+}
