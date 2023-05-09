9db61183f2b3c1e653aa067618f0bc373b736b5d
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-09 06:51:19 +0600

    add password feed back option for this project

--- Day 39 - Password Strength Background/script.js
+++ Day 39 - Password Strength Background/script.js
@@ -4,6 +4,7 @@ console.log('working ....');
 // all the variables
 const password = document.getElementById('password');
 const background = document.getElementById('background');
+const passwordInput = document.getElementById('password');
 
 // event listener for the input field
 password.addEventListener('input', (e) => {
@@ -18,4 +19,37 @@ document.getElementById('btn').addEventListener('click', () => {
 	password.select();
 	document.execCommand('copy');
 	alert('Password copied to clipboard');
+});
+
+// point: Password strength function
+function checkPasswordStrength(password) {
+	const regexList = [
+		/\d/, // Match a digit
+		/[a-z]/, // Match a lowercase letter
+		/[A-Z]/, // Match an uppercase letter
+		/[^A-Za-z0-9]/, // Match a symbol
+	];
+
+	let strength = 0;
+	regexList.forEach((regex) => {
+		if (regex.test(password)) {
+			strength++;
+		}
+	});
+
+	if (password.length >= 8 && strength >= 3) {
+		return 'strong';
+	} else if (password.length >= 6 && strength >= 2) {
+		return 'medium';
+	} else {
+		return 'weak';
+	}
+}
+
+// point : Password Strength Checker
+passwordInput.addEventListener('input', () => {
+	const password = passwordInput.value;
+	const passwordStrength = checkPasswordStrength(password);
+	const feedbackElement = document.querySelector('.password-strength-feedback');
+	feedbackElement.textContent = `Password strength: ${passwordStrength}`;
 });
