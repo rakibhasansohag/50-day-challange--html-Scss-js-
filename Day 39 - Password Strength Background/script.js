ff86f02896e8b4680eb71e8b105ccb3fb9ba974c
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-09 06:42:27 +0600

    add all the function for this project

--- Day 39 - Password Strength Background/script.js
+++ Day 39 - Password Strength Background/script.js
@@ -1,2 +1,21 @@
 'use strict';
 console.log('working ....');
+
+// all the variables
+const password = document.getElementById('password');
+const background = document.getElementById('background');
+
+// event listener for the input field
+password.addEventListener('input', (e) => {
+	const input = e.target.value;
+	const length = input.length;
+	const blurValue = 20 - length * 2;
+	background.style.filter = `blur(${blurValue}px)`;
+});
+
+// event listener for the button
+document.getElementById('btn').addEventListener('click', () => {
+	password.select();
+	document.execCommand('copy');
+	alert('Password copied to clipboard');
+});
