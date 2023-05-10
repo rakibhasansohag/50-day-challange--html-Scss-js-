0ba88dce04c6f3e27bf89726ed5f164323112931
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-11 01:20:18 +0600

    add style and all the function

--- Day 41 - Verify Account UI/script.js
+++ Day 41 - Verify Account UI/script.js
@@ -1,2 +1,20 @@
 'use strict';
 console.log('working ....');
+
+// point : all the variables
+
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+// point : all the functions
+codes.forEach((code, id) => {
+	code.addEventListener('keydown', (e) => {
+		if (e.key >= 0 && e.key <= 9) {
+			codes[id].value = '';
+			setTimeout(() => codes[id + 1].focus(), 10);
+		} else if (e.key === 'Backspace') {
+			setTimeout(() => codes[id - 1].focus(), 10);
+		}
+	});
+});
