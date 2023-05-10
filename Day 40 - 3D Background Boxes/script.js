012943206f58ae1075c03af566bf716a9504272f
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-10 09:51:15 +0600

    add 3d boxes dynamically

--- Day 40 - 3D Background Boxes/script.js
+++ Day 40 - 3D Background Boxes/script.js
@@ -1,2 +1,25 @@
 'use strict';
 console.log('working ....');
+
+// point : all the variable
+const boxesContainer = document.getElementById('boxes');
+const btn = document.getElementById('btn');
+
+// point : all the function
+function createBoxes() {
+	for (let i = 0; i < 4; i++) {
+		for (let j = 0; j < 4; j++) {
+			const box = document.createElement('div');
+			box.classList.add('box');
+			box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+			boxesContainer.appendChild(box);
+		}
+	}
+}
+
+createBoxes();
+
+// point : add event listener
+btn.addEventListener('click', () => {
+	boxesContainer.classList.toggle('big');
+});
