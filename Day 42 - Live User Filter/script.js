be4b013ea2e6ff57032442353867aa583d1ee972
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-11 10:44:45 +0600

    add no user found when user input invalid user name or location

--- Day 42 - Live User Filter/script.js
+++ Day 42 - Live User Filter/script.js
@@ -11,6 +11,8 @@ const listItems = [];
 
 // point : fetch random user and add money
 const getRandomUser = async () => {
+	result.innerHTML = `<li> Loading ... </li>`;
+
 	const res = await fetch('https://randomuser.me/api?results=50');
 	const { results } = await res.json();
 
@@ -28,6 +30,8 @@ const getRandomUser = async () => {
 		`;
 		result.appendChild(li);
 	});
+
+	filterData(filter.value);
 };
 
 getRandomUser();
@@ -37,11 +41,28 @@ getRandomUser();
 filter.addEventListener('input', (e) => filterData(e.target.value));
 
 function filterData(searchUser) {
+	const noUserFound = document.getElementById('no-user-found');
+	if (noUserFound) {
+		noUserFound.remove();
+	}
+
+	let count = 0;
+
 	listItems.forEach((item) => {
 		if (item.innerText.toLowerCase().includes(searchUser.toLowerCase())) {
 			item.classList.remove('hide');
+			count++;
 		} else {
 			item.classList.add('hide');
 		}
 	});
+
+	// point : check if no users are found and display a message
+
+	if (count === 0) {
+		const li = document.createElement('li');
+		li.setAttribute('id', 'no-user-found');
+		li.innerHTML = `<h3> No User Found </h3>`;
+		result.appendChild(li);
+	}
 }
