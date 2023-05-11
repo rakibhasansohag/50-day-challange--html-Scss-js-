99436c84833deb28d6b5d9f4c2cf8c693b979c0f
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-11 10:35:42 +0600

    add user and search functionalist

--- Day 42 - Live User Filter/script.js
+++ Day 42 - Live User Filter/script.js
@@ -1,2 +1,47 @@
 'use strict';
 console.log('working ....');
+// / resources : https://randomuser.me/
+
+// point : all the variables
+const result = document.getElementById('result');
+const filter = document.getElementById('filter');
+
+// point : initial value
+const listItems = [];
+
+// point : fetch random user and add money
+const getRandomUser = async () => {
+	const res = await fetch('https://randomuser.me/api?results=50');
+	const { results } = await res.json();
+
+	result.innerHTML = '';
+
+	results.forEach((user) => {
+		const li = document.createElement('li');
+		listItems.push(li);
+		li.innerHTML = `
+			<img src="${user.picture.large}" alt="${user.name.first}">
+			<div class="user-info">
+				<h4>${user.name.first} ${user.name.last}</h4>
+				<p>${user.location.city}, ${user.location.country}</p>
+				</div>
+		`;
+		result.appendChild(li);
+	});
+};
+
+getRandomUser();
+
+// point : search filter
+
+filter.addEventListener('input', (e) => filterData(e.target.value));
+
+function filterData(searchUser) {
+	listItems.forEach((item) => {
+		if (item.innerText.toLowerCase().includes(searchUser.toLowerCase())) {
+			item.classList.remove('hide');
+		} else {
+			item.classList.add('hide');
+		}
+	});
+}
