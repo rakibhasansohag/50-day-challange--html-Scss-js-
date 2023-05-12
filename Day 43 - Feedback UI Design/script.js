d2e7b542219dcefe7e5f449fd9b79f65fb05ed8d
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-12 23:17:30 +0600

    add all the functionalist for this project

--- Day 43 - Feedback UI Design/script.js
+++ Day 43 - Feedback UI Design/script.js
@@ -1,2 +1,37 @@
 'use strict';
 console.log('working ....');
+
+// point : all the variables
+
+const ratings = document.querySelectorAll('.rating');
+const sendBtn = document.querySelector('#send');
+const panel = document.querySelector('#panel');
+const ratingsContainer = document.querySelector('.ratings-container');
+let selectedRating = 'Satisfied';
+
+// point  : event listener
+ratingsContainer.addEventListener('click', (e) => {
+	if (e.target.parentNode.classList.contains('rating')) {
+		removeActive();
+		e.target.parentNode.classList.add('active');
+		selectedRating = e.target.nextElementSibling.innerHTML;
+		console.log(selectedRating);
+	}
+});
+
+function removeActive() {
+	for (let i = 0; i < ratings.length; i++) {
+		ratings[i].classList.remove('active');
+	}
+}
+
+sendBtn.addEventListener('click', (e) => {
+	panel.innerHTML = `
+		<i class="fas fa-heart"></i>
+		<strong>Thank You!</strong>
+		<br>
+		<strong>Feedback: ${selectedRating}</strong>
+		<br>
+		<p>We'll use your feedback to improve my work Flow</p>
+	`;
+});
