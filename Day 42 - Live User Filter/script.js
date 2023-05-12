541f2c536ea11d5845ee4d405035ba1ef2ec1e30
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-12 10:44:00 +0600

    add pagination

--- Day 42 - Live User Filter/script.js
+++ Day 42 - Live User Filter/script.js
@@ -1,68 +1,95 @@
 'use strict';
-console.log('working ....');
-// / resources : https://randomuser.me/
 
-// point : all the variables
+// Point: all the variables
 const result = document.getElementById('result');
 const filter = document.getElementById('filter');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
 
-// point : initial value
-const listItems = [];
+// Point: initial value
+let currentPage = 1;
+const usersPerPage = 5;
+let filteredUsers = [];
+let totalPages = 0;
 
-// point : fetch random user and add money
+// Point: fetch random user and add money
 const getRandomUser = async () => {
-	result.innerHTML = `<li> Loading ... </li>`;
-
 	const res = await fetch('https://randomuser.me/api?results=50');
 	const { results } = await res.json();
 
+	filteredUsers = results;
+	totalPages = Math.ceil(filteredUsers.length / usersPerPage);
+	showUsers();
+};
+
+// Point: Show users based on the current page
+const showUsers = () => {
 	result.innerHTML = '';
 
-	results.forEach((user) => {
+	if (filteredUsers.length === 0) {
+		result.innerHTML = '<li class="no-results">No users found.</li>';
+		return;
+	}
+
+	const startIndex = (currentPage - 1) * usersPerPage;
+	const endIndex = startIndex + usersPerPage;
+	const usersToShow = filteredUsers.slice(startIndex, endIndex);
+
+	usersToShow.forEach((user) => {
 		const li = document.createElement('li');
-		listItems.push(li);
 		li.innerHTML = `
-			<img src="${user.picture.large}" alt="${user.name.first}">
-			<div class="user-info">
-				<h4>${user.name.first} ${user.name.last}</h4>
-				<p>${user.location.city}, ${user.location.country}</p>
-				</div>
-		`;
+      <img src="${user.picture.large}" alt="${user.name.first}">
+      <div class="user-info">
+        <h4>${user.name.first} ${user.name.last}</h4>
+        <p>${user.location.city}, ${user.location.country}</p>
+      </div>
+    `;
 		result.appendChild(li);
 	});
 
-	filterData(filter.value);
+	// Update pagination
+	updatePagination();
+};
+
+// Point: Update pagination buttons
+const updatePagination = () => {
+	prevBtn.disabled = currentPage === 1;
+	nextBtn.disabled = currentPage === totalPages;
+};
+
+// Point: Go to the previous page
+const prevPage = () => {
+	if (currentPage > 1) {
+		currentPage--;
+		showUsers();
+	}
+};
+
+// Point: Go to the next page
+const nextPage = () => {
+	if (currentPage < totalPages) {
+		currentPage++;
+		showUsers();
+	}
 };
 
 getRandomUser();
 
-// point : search filter
+// Point: search filter
+filter.addEventListener('input', (e) => {
+	const searchTerm = e.target.value.toLowerCase();
+	filteredUsers = searchTerm
+		? filteredUsers.filter((user) => {
+				const fullName = `${user.name.first} ${user.name.last}`.toLowerCase();
+				return fullName.includes(searchTerm);
+		  })
+		: getRandomUser();
 
-filter.addEventListener('input', (e) => filterData(e.target.value));
+	currentPage = 1;
+	totalPages = Math.ceil(filteredUsers.length / usersPerPage);
+	showUsers();
+});
 
-function filterData(searchUser) {
-	const noUserFound = document.getElementById('no-user-found');
-	if (noUserFound) {
-		noUserFound.remove();
-	}
-
-	let count = 0;
-
-	listItems.forEach((item) => {
-		if (item.innerText.toLowerCase().includes(searchUser.toLowerCase())) {
-			item.classList.remove('hide');
-			count++;
-		} else {
-			item.classList.add('hide');
-		}
-	});
-
-	// point : check if no users are found and display a message
-
-	if (count === 0) {
-		const li = document.createElement('li');
-		li.setAttribute('id', 'no-user-found');
-		li.innerHTML = `<h3> No User Found </h3>`;
-		result.appendChild(li);
-	}
-}
+// Point: Previous and Next buttons
+prevBtn.addEventListener('click', prevPage);
+nextBtn.addEventListener('click', nextPage);
