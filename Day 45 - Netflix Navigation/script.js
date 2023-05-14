366ed689543ce1eb2ebee2a16b67115e53b6d99a
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-14 15:35:51 +0600

    add all the functiionalist for navigation

--- Day 45 - Netflix Navigation/script.js
+++ Day 45 - Netflix Navigation/script.js
@@ -1,2 +1,19 @@
 'use strict';
 console.log('working ....');
+
+// point : all the variables
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const nav = document.querySelectorAll('.nav');
+
+// point : add event listener to open button
+openBtn.addEventListener('click', () => {
+	nav.forEach((navEl) => navEl.classList.add('visible'));
+});
+
+// point : add event listener to close button
+closeBtn.addEventListener('click', () => {
+	nav.forEach((navEl) => navEl.classList.remove('visible'));
+});
+
+//
