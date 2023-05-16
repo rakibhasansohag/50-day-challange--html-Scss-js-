448d174ea1db6149621079d4708aa0efcf07de1d
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-17 01:18:15 +0600

    add style and funciton

--- Day 48 - Random Image Feed/script.js
+++ Day 48 - Random Image Feed/script.js
@@ -1,2 +1,25 @@
 'use strict';
 console.log('working ....');
+
+// point: all the variables
+const container = document.querySelector('.container');
+const unsplashUrl = 'https://source.unsplash.com/user/wsanter/';
+const rows = 10;
+
+// point: all the functions
+
+for (let i = 0; i < rows * 3; i++) {
+	const img = document.createElement('img');
+	img.src = `${unsplashUrl}${getRandomSize()}`;
+	container.appendChild(img);
+}
+
+function getRandomSize() {
+	return `${getRandomNumber()}x${getRandomNumber()}`;
+}
+
+function getRandomNumber() {
+	return Math.floor(Math.random() * 10) + 300;
+}
+
+// point: all the event listeners
