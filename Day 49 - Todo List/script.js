5d9f56449a33e8b7d31488471bbc076b884f8586
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2023-05-18 00:43:29 +0600

    changed the ui and some functiionalist

--- Day 49 - Todo List/script.js
+++ Day 49 - Todo List/script.js
@@ -1,72 +1,106 @@
 'use strict';
+
 console.log('working ....');
 
-// point : all the variable
-
+// Point: All the variables
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todosUl = document.getElementById('todos');
-const todos = JSON.parse(localStorage.getItem('todos'));
+let todos = JSON.parse(localStorage.getItem('todos'));
 
-// point : todos
+input.focus();
+// Point: Todos
 if (todos) {
-	todos.forEach((todo) => addTodo(todo));
+	todos.forEach((todo) => addTodoElement(todo));
 } else {
-	console.log('no todos');
+	todos = [];
+	console.log('No todos');
 }
 
-// point : all the event listener
+// Point: All the event listeners
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	addTodo();
 });
 
-function addTodo(todo) {
-	let todoText = input.value;
-	if (todo) {
-		todoText = todo.text;
-	}
-
+function addTodo() {
+	const todoText = input.value.trim();
 	if (todoText) {
-		const todoEl = document.createElement('li');
-		if (todo && todo.completed) {
-			todoEl.classList.add('completed');
-		}
-		todoEl.innerText = todoText;
-
-		todoEl.addEventListener('click', () => {
-			todoEl.classList.toggle('completed'), updateLS();
-		});
-
-		todoEl.addEventListener('contextmenu', (e) => {
-			e.preventDefault();
-			todoEl.remove();
-			updateLS();
-		});
-
-		todosUl.appendChild(todoEl);
-
+		const todo = {
+			id: Date.now(),
+			text: todoText,
+			completed: false,
+		};
+		todos.push(todo);
+		addTodoElement(todo);
+		updateLocalStorage();
 		input.value = '';
-
-		updateLS();
 	}
 }
 
-function updateLS() {
-	const todosEl = document.querySelectorAll('li');
+function addTodoElement(todo) {
+	const todoElement = document.createElement('li');
+	todoElement.innerHTML = `
+    <span class="todo-text">${todo.text}</span>
+    <div class="todo-options">
+      <i class="fas fa-check-circle"></i>
+      <i class="fas fa-trash"></i>
+    </div>
+  `;
+	todoElement.classList.add('todo-item');
+	if (todo.completed) {
+		todoElement.classList.add('completed');
+	}
 
-	const todos = [];
-
-	todosEl.forEach((todoEl) => {
-		todos.push({
-			text: todoEl.innerText,
-			completed: todoEl.classList.contains('completed'),
-		});
+	const todoText = todoElement.querySelector('.todo-text');
+	todoText.addEventListener('click', () => {
+		toggleTodoCompletion(todo.id);
 	});
 
+	const todoOptions = todoElement.querySelector('.todo-options');
+	const completionIcon = todoOptions.querySelector('.fa-check-circle');
+	completionIcon.addEventListener('click', (e) => {
+		e.stopPropagation(); // / Prevent the click event from bubbling up to the todoText element
+		toggleTodoCompletion(todo.id);
+	});
+
+	const deleteIcon = todoOptions.querySelector('.fa-trash');
+	deleteIcon.addEventListener('click', (e) => {
+		e.stopPropagation();
+		deleteTodoElement(todo.id);
+		updateLocalStorage();
+	});
+
+	todoElement.addEventListener('contextmenu', (e) => {
+		e.preventDefault();
+		deleteTodoElement(todo.id);
+		updateLocalStorage();
+	});
+
+	todosUl.appendChild(todoElement);
+}
+function toggleTodoCompletion(todoId) {
+	todos = todos.map((todo) => {
+		if (todo.id === todoId) {
+			return { ...todo, completed: !todo.completed };
+		}
+		return todo;
+	});
+	updateLocalStorage();
+	renderTodos();
+}
+
+function deleteTodoElement(todoId) {
+	todos = todos.filter((todo) => todo.id !== todoId);
+	updateLocalStorage();
+	renderTodos();
+}
+
+function updateLocalStorage() {
 	localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-// localStorage
-
-// localStorage.setItem('name', JSON.stringify(obj));
+function renderTodos() {
+	todosUl.innerHTML = '';
+	todos.forEach((todo) => addTodoElement(todo));
+}
